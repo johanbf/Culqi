0ae451266649cd046700a2eb1a31c8ad8c473f7a
Johan Andrew Buitron Flores <buitron19.18@gmail.com>
2023-12-17 16:16:04 -0500

    cambio

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 </script>
 
 <template>
   <router-view />
-  <SpeedInsights />
 </template>
 
 <style scoped>
