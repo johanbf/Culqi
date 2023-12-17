0931097b2a12ba9ce0d1bff9ecf5315e3d143c73
Johan Andrew Buitron Flores <buitron19.18@gmail.com>
2023-12-17 16:17:04 -0500

    imagen

--- src/components/ListServices.vue
+++ src/components/ListServices.vue
@@ -22,7 +22,7 @@
     <router-link :to="'historial-ganancias-operaciones'">
       <div class="bg-grayCulqi p-5 rounded flex items-center mb-5">
         <div class="p-2 bg-orange rounded mr-7">
-          <img :alt="'image Historial de ganancias y operaciones' " :src="'assets/svg/calendar.svg'" height="24" width="24">
+          <img :alt="'image Historial de ganancias y operaciones' " :src="'../assets/svg/calendar.svg'" height="24" width="24">
         </div>
         <p class="text-card">Historial de ganancias y operaciones</p>
       </div>
