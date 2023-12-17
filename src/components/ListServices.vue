0a46c06a73152608fdc5b1296062489a0a848435
Johan Andrew Buitron Flores <buitron19.18@gmail.com>
2023-12-17 16:08:28 -0500

    subida

--- src/components/ListServices.vue
+++ src/components/ListServices.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
-  import CardService from "./CardService.vue";
-
-  const phone = import('../assets/svg/phone.svg')
-  const lightbulb = import('../assets/svg/lightbulb.svg')
-  const calendar = import('../assets/svg/calendar.svg')
-
-  let services : Array<Services> = [
-    {
-      img: phone,
-      title: 'Recargas',
-      ruta: 'recargas'
-    },
-    {
-      img: lightbulb,
-      title: 'Pago de servicios',
-      ruta: 'pago-de-servicios'
-    },
-    {
-      img: calendar,
-      title: 'Historial de ganancias y operaciones',
-      ruta: 'historial-ganancias-operaciones'
-    }
-  ]
 </script>
 
 <template>
   <div class="mt-4">
-    <CardService v-for="(service ) in services" :img="service.img" :name-service="service.title" :ruta="service.ruta" :key="service.title"/>
+    <router-link to="recargas">
+      <div class="bg-grayCulqi p-5 rounded flex items-center mb-5">
+        <div class="p-2 bg-orange rounded mr-7">
+          <img alt="image recargas " src="../assets/svg/phone.svg" height="24" width="24">
+        </div>
+        <p class="text-card">Recargas</p>
+      </div>
+    </router-link>
+    <router-link :to="'pago-de-servicios'">
+      <div class="bg-grayCulqi p-5 rounded flex items-center mb-5">
+        <div class="p-2 bg-orange rounded mr-7">
+          <img :alt="'image Pago de servicios' " src="../assets/svg/lightbulb.svg" height="24" width="24">
+        </div>
+        <p class="text-card">Pago de servicios</p>
+      </div>
+    </router-link>
+    <router-link :to="'historial-ganancias-operaciones'">
+      <div class="bg-grayCulqi p-5 rounded flex items-center mb-5">
+        <div class="p-2 bg-orange rounded mr-7">
+          <img :alt="'image Historial de ganancias y operaciones' " :src="'assets/svg/calendar.svg'" height="24" width="24">
+        </div>
+        <p class="text-card">Historial de ganancias y operaciones</p>
+      </div>
+    </router-link>
   </div>
 </template>
 
